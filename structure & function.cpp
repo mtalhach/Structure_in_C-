5d9c516300f0f58416d7318034e366f020381527
CH TALHA <100463587+mtalhach@users.noreply.github.com>
2022-04-12 23:10:53 +0500

    struct & function

---
+++ structure & function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+struct person
+{
+    char name[10];
+    int age;
+    float salary;
+};
+
+void display(person p)
+{
+    cout << "Name is: " << p.name << endl;
+    cout << "Age is: " << p.age << endl;
+    cout << "Salary is: " << p.salary << endl;
+}
+int main()
+{
+    person p;
+    cout << "Enter The name of Person: ";
+    // cin >> p.name;
+    cin.get(p.name, 10);
+    cout << "Enter The Age of Person: ";
+    cin >> p.age;
+    cout << "Enter The Salary of Person: ";
+    cin >> p.salary;
+    display(p); // mrthod call
+}
